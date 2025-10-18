08609c30c5162baf430bd733302e46000e2efb78
KondouMasaki <48499002+KondouMasaki@users.noreply.github.com>
2025-10-18 14:18:07 +0900

    add back links

---
+++ q-hitofude/map.js
@@ -0,0 +1,126 @@
+var Map = function() {
+};
+Map.prototype = 
+// %%=start
+{
+	"map": [
+		[1,1,1,1,1,1,1,1,1,1,1,1],
+		[1,1,1,1,1,1,1,1,1,1,1,1],
+		[1,1,1,1,1,1,1,1,1,1,1,1],
+		[1,1,1,1,0,0,0,5,1,1,1,1],
+		[1,1,1,1,0,1,1,0,1,1,1,1],
+		[1,1,1,1,5,1,1,5,1,1,1,1],
+		[1,1,1,1,0,0,0,0,1,1,1,1],
+		[1,1,1,1,1,1,1,1,1,1,1,1],
+		[1,1,1,1,1,1,1,1,1,1,1,1],
+		[1,1,1,1,1,1,1,1,1,1,1,1],
+		[1,1,1,1,1,1,1,1,1,1,1,1],
+		[1,1,1,1,1,1,1,1,1,1,1,1]
+	],
+	"start": {
+		"x": 7,
+		"y": 4,
+		"direction": 0,
+		"life": 65535,
+		"speed": 1,
+		"soft": false
+	},
+	"hint": "一筆書きで %goals% このゴールへ行こう",
+	"state": 0,
+	"goals": 3,
+	"patterns": 1,
+	"blocksLimit": 0,
+	"links": {
+		"question": "シンプル",
+		"previous": "",
+		"next": "",
+		"genurl": "https://kondoumasaki.github.io/create-robot-mondai/q-hitofude/render.html"
+	},
+	"robot": {
+		"type": 0,
+		"Basic": {
+			"forward": true,
+			"turn_right": true,
+			"turn_left": true,
+			"nop": true
+		},
+		"Standard": {
+			"floor_color_is": true,
+			"robot_direction_is": true,
+			"movable_is": true
+		},
+		"Advanced": {
+			"times_loop": true,
+			"floor_color_loop": true,
+			"movable_loop": true
+		},
+		"Expert": {
+			"write_register": true,
+			"read_register": true,
+			"get_floor_color": true,
+			"get_direction": true
+		},
+		"Enhanced": {
+			"values_equal_is": true,
+			"values_equal_loop": true,
+			"infinity_loop": true,
+			"is_movable_to": true
+		},
+		"Superior": {
+			"add_register": true,
+			"sub_register": true,
+			"add_register_index": true,
+			"sub_register_index": true,
+			"set_register_index": true,
+			"get_register_index": true
+		},
+		"Replete": {
+			"read_cell_value": true,
+			"read_cell_value_index": true,
+			"write_cell_value": true,
+			"values_compare": true,
+			"expression_if": true,
+			"expression_loop": true
+		},
+		"Master": {
+		}
+	},
+	"chars": [
+		[ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 ],
+		[ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 ],
+		[ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 ],
+		[ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 ],
+		[ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 ],
+		[ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 ],
+		[ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 ],
+		[ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 ],
+		[ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 ],
+		[ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 ],
+		[ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 ],
+		[ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 ]
+	],
+	"hintBlocks": '',
+	"map2": [],
+	"chars2": [],
+	
+	"image_file_dir": '../img/'
+}
+// end=%%
+;
+
+/**
+ * コード実行前の処理
+ */
+Map.prototype.beforeStart = function(pattern) {
+	// if pettern is <empty string> selected "ぜんぶ"
+};
+/**
+ * ターンごとに発生する処理
+ */
+Map.prototype.afterMoved = function(t, pos) {
+	// t is turns value, pos is robot info { "x": num, "y": num, "direction": num }
+	if (Map.prototype.map[pos.y][pos.x] == Map.prototype.colorValue.yellow) {
+		const bp = Control.prototype.getRobotBackPosition();
+		Map.prototype.map[bp.y][bp.x] = Map.prototype.colorValue.black;
+	}
+};
